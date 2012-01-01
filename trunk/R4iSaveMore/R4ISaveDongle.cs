b11c2674d19c3267785fd95696d4f5fa22341005
eli.sherer@gmail.com <eli.sherer@gmail.com@d678f0f5-0f62-52f0-88cc-f9cc2cb8a1f8>
2012-01-01 18:24:10 +0000

    first commit

---
+++ trunk/R4iSaveMore/R4ISaveDongle.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using HIDCommunications;
+
+namespace R4iSaveMore
+{
+    internal class R4ISaveDongle : HIDCommControl
+    {
+        public struct GameInformation
+        {
+            public string Name;
+            public int SaveFlashSize;
+        }
+
+        private static readonly byte[] StaticBuffer = new byte[0x41];
+
+        private const int R4ISaveDongleVID = 0x04D8;
+        private const int R4ISaveDonglePID = 0x003F;
+
+        private static readonly byte[] StartDownloadBytes = { 0x11, 0x11, 0x03};
+        private const byte StartDownloadResponse = 0;
+
+        private static readonly byte[] GetHeaderBytes = { 0x22, 0x22, 0x00 };
+        private const byte GetHeaderResponse = 10;
+
+        private static readonly byte[] StopBytes = { 0x2f, 0x2f, 0x00 };
+        private const byte StopResponse = 0;
+
+        private static readonly byte[] Get512Bytes = { 0x33, 0x33, 0x03, 0, 0, 0, 0, 0, 0, 0, 0 }; //and u24 - offset (BE-msB first)
+        private const byte Get512Response = 8; //after 'Start' and before 'Stop'
+        
+        private static readonly byte[] WriteBytes = { 0x44, 0x44, 0x00, 0x02 }; //u24 - offset and 0x20 of data
+        private const byte WriteResponse = 0; //after 'Start' and before 'Stop'
+
+        private static readonly byte[] ClearRamBytes = { 0x66, 0x66, 0x00 , 0x01, 0x00};
+        private const byte ClearRamResponse = 0;
+
+        private static readonly byte[] CheckBytes = { 0xa0, 0x00, 0x00 };
+        private const byte CheckResponse = 3;
+
+        private BackgroundWorker _backgroundWorker;
+        private bool _busy;
+
+        public delegate void Int32EventHandler(Object sender, int value);
+
+        public event Int32EventHandler ProgressChanged;
+        public event Int32EventHandler DownloadComplete;
+
+        public R4ISaveDongle(): base(R4ISaveDongleVID, R4ISaveDonglePID)
+        {}
+
+        public R4ISaveDongle(int vid, int pid) : base(vid, pid) //might be used to do the upgrade thing
+        {}
+
+        public byte[] SendCommand(byte[] array, int readBlocks)
+        {
+            var outputBuffer = new byte[(0x40+1) * readBlocks];
+            var returnBuffer = new byte[0x40*readBlocks];
+            StaticBuffer[0] = 0;
+            var sizeToWrite = array.Length <= 0x40 ? array.Length : 0x40;
+            Buffer.BlockCopy(array, 0, StaticBuffer, 1, sizeToWrite);
+            var success = writeRawReportToDevice(StaticBuffer);
+            if (success && readBlocks > 0)
+            {   success = readMultipleReportsFromDevice(ref outputBuffer, readBlocks);
+                if (success)
+                    for (var i=0;i<readBlocks;i++)
+                        Buffer.BlockCopy(outputBuffer,1+0x41*i,returnBuffer,i*0x40,0x40);
+            }
+            return success ? returnBuffer : null ;
+        }
+
+        public int R4IGetVersion()
+        {
+            var retBuffer = SendCommand(CheckBytes, CheckResponse);
+            return retBuffer[2];
+        }
+
+        public GameInformation R4IGetGameName()
+        {
+            var info = new GameInformation();
+            var retBuffer = SendCommand(GetHeaderBytes, GetHeaderResponse);
+            var stringBytes = new byte[12];
+            if (retBuffer == null)
+                return info;
+            Buffer.BlockCopy(retBuffer,0x40,stringBytes,0,stringBytes.Length);
+            info.Name = stringBytes[0] != 0 ? Encoding.ASCII.GetString(stringBytes) : "0x" + retBuffer[0x240].ToString("X") + retBuffer[0x241].ToString("X") + retBuffer[0x242].ToString("X");
+            if (stringBytes[0] == 0) //3DS ?
+                info.SaveFlashSize = retBuffer[0x242] == 0x11 ? 0x20000 : retBuffer[0x242] == 0x13 ? 0x80000 : 0;
+            else
+            {
+                info.SaveFlashSize = 512; //Don't know ???
+            }
+            
+            return info;
+        }
+
+        public void R4IDownloadSave(string filePath)
+        {
+            if (_busy)
+            {
+                if (DownloadComplete != null)
+                    DownloadComplete(this, -2); //send error -2
+                return;
+            }
+            _backgroundWorker = new BackgroundWorker { WorkerReportsProgress = true };
+            _backgroundWorker.ProgressChanged += BackgroundWorkerProgressChanged;
+            _backgroundWorker.DoWork += BackgroundWorkerDoWork;
+            _backgroundWorker.RunWorkerCompleted += BackgroundWorkerRunWorkerCompleted;
+            _backgroundWorker.RunWorkerAsync(filePath);
+            _busy = true;
+        }
+
+        #region Background Worker methods
+
+        private void BackgroundWorkerDoWork(object sender, DoWorkEventArgs e)
+        {
+            var filePath = e.Argument.ToString();
+            // Get game's file size
+            var gameInfo = R4IGetGameName();
+            if (gameInfo.SaveFlashSize <= 0) { e.Result = -1; return; }          
+            // Prepare buffers
+            var commandBuffer = new byte[Get512Bytes.Length];
+            Buffer.BlockCopy(Get512Bytes, 0, commandBuffer, 0, Get512Bytes.Length);
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            var fs = File.OpenWrite(filePath);
+            // Start the process
+            SendCommand(StopBytes, StopResponse);
+            SendCommand(ClearRamBytes, ClearRamResponse);
+            SendCommand(StartDownloadBytes, StartDownloadResponse);
+            while (fs.Length < gameInfo.SaveFlashSize)
+            {
+                commandBuffer[3] = (byte)((fs.Length >> 16) & 0xFF); // MSB - BigE
+                commandBuffer[4] = (byte)((fs.Length >> 8) & 0xFF);
+                commandBuffer[5] = (byte)(fs.Length & 0xFF);        //LSB
+                var response = SendCommand(commandBuffer, Get512Response);
+                if (response == null) { fs.Close(); e.Result = -1; return; } //Exit with error
+                //Write the information to the file
+                fs.Write(response, 0, response.Length);
+                _backgroundWorker.ReportProgress((int)(100 * fs.Length / gameInfo.SaveFlashSize));
+            }
+            fs.Close();
+            commandBuffer[0] = 0x2f;
+            commandBuffer[1] = 0x2f;
+            SendCommand(commandBuffer, StopResponse); // Stop (we don't care if it fails...)
+            e.Result = 0;
+        }
+
+        private void BackgroundWorkerProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProgressChanged != null)
+                ProgressChanged(this, e.ProgressPercentage);
+        }
+        private void BackgroundWorkerRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            _busy = false;
+            if (DownloadComplete != null)
+                DownloadComplete(this, (int)e.Result);
+        }
+        #endregion
+
+
+    }
+}
---
+++ trunk/R4iSaveMore/frmR4iSaveMore.designer.cs
@@ -0,0 +1,283 @@
+﻿namespace R4iSaveMore
+{
+    partial class frmR4iSaveMore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmR4iSaveMore));
+            this.grpSandbox = new System.Windows.Forms.GroupBox();
+            this.pbDevice = new System.Windows.Forms.ProgressBar();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnFWVer = new System.Windows.Forms.Button();
+            this.lblFw = new System.Windows.Forms.Label();
+            this.lblSaveSize = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnGameInfo = new System.Windows.Forms.Button();
+            this.cmbPre = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPre = new System.Windows.Forms.Label();
+            this.numRead = new System.Windows.Forms.NumericUpDown();
+            this.lblRead = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.grpSandbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpSandbox
+            // 
+            this.grpSandbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpSandbox.Controls.Add(this.pbDevice);
+            this.grpSandbox.Controls.Add(this.btnDownload);
+            this.grpSandbox.Controls.Add(this.btnFWVer);
+            this.grpSandbox.Controls.Add(this.lblFw);
+            this.grpSandbox.Controls.Add(this.lblSaveSize);
+            this.grpSandbox.Controls.Add(this.lblName);
+            this.grpSandbox.Controls.Add(this.btnGameInfo);
+            this.grpSandbox.Controls.Add(this.cmbPre);
+            this.grpSandbox.Controls.Add(this.label1);
+            this.grpSandbox.Controls.Add(this.lblPre);
+            this.grpSandbox.Controls.Add(this.numRead);
+            this.grpSandbox.Controls.Add(this.lblRead);
+            this.grpSandbox.Controls.Add(this.txtSend);
+            this.grpSandbox.Controls.Add(this.btnGo);
+            this.grpSandbox.Controls.Add(this.txtOutput);
+            this.grpSandbox.Enabled = false;
+            this.grpSandbox.Location = new System.Drawing.Point(12, 12);
+            this.grpSandbox.Name = "grpSandbox";
+            this.grpSandbox.Size = new System.Drawing.Size(500, 238);
+            this.grpSandbox.TabIndex = 1;
+            this.grpSandbox.TabStop = false;
+            this.grpSandbox.Text = "R4i SaveDongle";
+            // 
+            // pbDevice
+            // 
+            this.pbDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbDevice.Location = new System.Drawing.Point(6, 184);
+            this.pbDevice.Name = "pbDevice";
+            this.pbDevice.Size = new System.Drawing.Size(487, 15);
+            this.pbDevice.TabIndex = 11;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Location = new System.Drawing.Point(383, 205);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(111, 27);
+            this.btnDownload.TabIndex = 10;
+            this.btnDownload.Text = "Download from Cart";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnFWVer
+            // 
+            this.btnFWVer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFWVer.Location = new System.Drawing.Point(157, 205);
+            this.btnFWVer.Name = "btnFWVer";
+            this.btnFWVer.Size = new System.Drawing.Size(76, 27);
+            this.btnFWVer.TabIndex = 9;
+            this.btnFWVer.Text = "FW Version";
+            this.btnFWVer.UseVisualStyleBackColor = true;
+            this.btnFWVer.Click += new System.EventHandler(this.btnFWVer_Click);
+            // 
+            // lblFw
+            // 
+            this.lblFw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFw.AutoSize = true;
+            this.lblFw.Location = new System.Drawing.Point(239, 205);
+            this.lblFw.Name = "lblFw";
+            this.lblFw.Size = new System.Drawing.Size(51, 13);
+            this.lblFw.TabIndex = 8;
+            this.lblFw.Text = "<fw_ver>";
+            // 
+            // lblSaveSize
+            // 
+            this.lblSaveSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblSaveSize.AutoSize = true;
+            this.lblSaveSize.Location = new System.Drawing.Point(88, 219);
+            this.lblSaveSize.Name = "lblSaveSize";
+            this.lblSaveSize.Size = new System.Drawing.Size(63, 13);
+            this.lblSaveSize.TabIndex = 8;
+            this.lblSaveSize.Text = "<save size>";
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(88, 205);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "<name>";
+            // 
+            // btnGameInfo
+            // 
+            this.btnGameInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGameInfo.Location = new System.Drawing.Point(6, 205);
+            this.btnGameInfo.Name = "btnGameInfo";
+            this.btnGameInfo.Size = new System.Drawing.Size(76, 27);
+            this.btnGameInfo.TabIndex = 7;
+            this.btnGameInfo.Text = "Game Info";
+            this.btnGameInfo.UseVisualStyleBackColor = true;
+            this.btnGameInfo.Click += new System.EventHandler(this.btnGameInfo_Click);
+            // 
+            // cmbPre
+            // 
+            this.cmbPre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbPre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPre.FormattingEnabled = true;
+            this.cmbPre.Items.AddRange(new object[] {
+            "Check Version (receives 0xC0)",
+            "Get Rom Header (receives 0x400)",
+            "Clear (doesn\'t receive)",
+            "Start Save Download (doesn\'t receive)",
+            "Get media From save at offset 0 (receives 0x200)",
+            "Stop Download (doesn\'t receive)"});
+            this.cmbPre.Location = new System.Drawing.Point(71, 27);
+            this.cmbPre.Name = "cmbPre";
+            this.cmbPre.Size = new System.Drawing.Size(423, 21);
+            this.cmbPre.TabIndex = 6;
+            this.cmbPre.SelectedIndexChanged += new System.EventHandler(this.cmbPre_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Command:";
+            // 
+            // lblPre
+            // 
+            this.lblPre.AutoSize = true;
+            this.lblPre.Location = new System.Drawing.Point(6, 30);
+            this.lblPre.Name = "lblPre";
+            this.lblPre.Size = new System.Drawing.Size(61, 13);
+            this.lblPre.TabIndex = 5;
+            this.lblPre.Text = "Predefined:";
+            // 
+            // numRead
+            // 
+            this.numRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numRead.Location = new System.Drawing.Point(355, 59);
+            this.numRead.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numRead.Name = "numRead";
+            this.numRead.Size = new System.Drawing.Size(58, 20);
+            this.numRead.TabIndex = 4;
+            // 
+            // lblRead
+            // 
+            this.lblRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRead.AutoSize = true;
+            this.lblRead.Location = new System.Drawing.Point(286, 63);
+            this.lblRead.Name = "lblRead";
+            this.lblRead.Size = new System.Drawing.Size(63, 13);
+            this.lblRead.TabIndex = 3;
+            this.lblRead.Text = "Read back:";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSend.Location = new System.Drawing.Point(69, 60);
+            this.txtSend.MaxLength = 128;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(211, 20);
+            this.txtSend.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(419, 59);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 20);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Send";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 8.25F);
+            this.txtOutput.Location = new System.Drawing.Point(6, 86);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(488, 90);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // frmR4iSaveMore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 262);
+            this.Controls.Add(this.grpSandbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(540, 300);
+            this.Name = "frmR4iSaveMore";
+            this.Text = "R4iSaveMore";
+            this.grpSandbox.ResumeLayout(false);
+            this.grpSandbox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRead)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpSandbox;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.NumericUpDown numRead;
+        private System.Windows.Forms.Label lblRead;
+        private System.Windows.Forms.Label lblPre;
+        private System.Windows.Forms.ComboBox cmbPre;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnGameInfo;
+        private System.Windows.Forms.Button btnFWVer;
+        private System.Windows.Forms.Label lblFw;
+        private System.Windows.Forms.Label lblSaveSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbDevice;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
